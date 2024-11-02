add80e3db45cd29ef78be606f3e9ebf436566e2b
imAshishSaini <19bcs1363@gmail.com>
2024-11-02 22:34:41 +0530

    first

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
 const incomingRequestLogger = (req, res, next) => {
-    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-    fs.appendFileSync('log.txt', req.method + " " + req.url+ " " + ip  + " " + new Date().toISOString() + "\n")
+    // var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    // fs.appendFileSync('log.txt', req.method + " " + req.url+ " " + ip  + " " + new Date().toISOString() + "\n")
     next()
 }
 
