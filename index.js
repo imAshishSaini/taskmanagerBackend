f9671d3a29a0b7c8eb87d274e50c6b1011a478a2
imAshishSaini <19bcs1363@gmail.com>
2024-11-03 08:12:38 +0530

    update

--- index.js
+++ index.js
@@ -14,13 +14,17 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(incomingRequestLogger)
 
 app.use('/api/user', userRouter)
-app.use('/api/task', taskRouter)
+app.use('/api/task', taskRouter) 
 
-mongoose.connect(process.env.MONGOOSE_URI_STRING, {})
-.then(() => console.log('Connected to database'))
-.catch((err) => console.log('Error connecting to database', err))
+mongoose.connect(process.env.MONGOOSE_URI_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+  })
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log('Error connecting to database', err));
+
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started on port', process.env.PORT || 3000))
